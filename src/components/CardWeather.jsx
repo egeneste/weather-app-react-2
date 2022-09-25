2ae3fc601534974e24b761d27ea63ee80bb02cc9
egeneste <elzr.geneste@gmail.com>
2022-09-25 16:00:04 -0400

    loading image and backgound for main page

--- src/components/CardWeather.jsx
+++ src/components/CardWeather.jsx
@@ -1,75 +1,103 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loading from './Loading';
 
-const CardWeather = ({lat, lon}) => {
-    
-  const [weather, setWeather] =  useState();
-  const [temperature, setTemperature] = useState('')
-  const [tempUnit, setTempUnit] = useState("c")
-  const [toggleTempUnit, setToggleTempUnit] = useState(false)
+const CardWeather = ({ lat, lon }) => {
+  const [weather, setWeather] = useState();
+  const [temperature, setTemperature] = useState('');
+  const [tempUnit, setTempUnit] = useState('c');
+  const [toggleTempUnit, setToggleTempUnit] = useState(false);
 
-    const getWeatherInfo = async (URL) => {
-        const weatherInfo = await axios.get(URL)
-      
-        setWeather(weatherInfo.data)
-        setTempUnit('c')
+  const getWeatherInfo = async (URL) => {
+    const weatherInfo = await axios.get(URL);
+
+    setWeather(weatherInfo.data);
+    setTempUnit('c');
+  };
+  useEffect(() => {
+    if (lat && lon) {
+      const API_KEY = '9c51206483db78dcb6a95612a895e3c8';
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+      getWeatherInfo(URL);
     }
-    useEffect(() => {
-        if (lat && lon){
-            const API_KEY = '9c51206483db78dcb6a95612a895e3c8'
-            const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-            getWeatherInfo(URL)
-        }
-        
-    }, [lat, lon])
-  
-    useEffect(() => {
-      if (tempUnit === 'f'){
-        setTemperature(`${Math.round(((parseFloat(weather?.main.temp) - 273.15) * 9 / 5 + 32 ) *100)/100}  °F`)
-      }
-      else {
-        setTemperature(`${Math.round((parseFloat(weather?.main.temp) - 273.15)*100)/100}  °C`)
-      }
+  }, [lat, lon]);
 
-    },[tempUnit])
+  useEffect(() => {
+    if (tempUnit === 'f') {
+      setTemperature(
+        `${
+          Math.round(
+            (((parseFloat(weather?.main.temp) - 273.15) * 9) / 5 + 32) * 100
+          ) / 100
+        }  °F`
+      );
+    } else {
+      setTemperature(
+        `${
+          Math.round((parseFloat(weather?.main.temp) - 273.15) * 100) / 100
+        }  °C`
+      );
+    }
+  }, [tempUnit]);
   const handle_unit_changed = () => {
     if (tempUnit === 'f') {
-      setTempUnit('c')
+      setTempUnit('c');
+    } else {
+      setTempUnit('f');
     }
-    else {
-      setTempUnit("f")
-    }
-    
-  }
+  };
   return (
     <>
-      {weather ? 
-      <div className='card'>
-        <h2>Weather App</h2>
-        <p>{weather.name}, {weather.sys.country}</p>
-       
-        <div className='weather'>
-          <div className='image'>
-            <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="code" />
-          </div>
-          <div className="weather-datos">
-            <p>&#34;{weather.weather[0].description}&#34;</p>
-            <p>Wind speed : <span>{weather.wind.speed} m/s</span></p>
-            <p>Clouds: <span>{weather.clouds.all} %</span></p>
-            <p>Pressure: <span>{weather.main.pressure} hPa</span></p>
-          </div>
-        </div>
-        
-        <p className="temperature">{tempUnit == "c" ? `${Math.round(((parseFloat(weather?.main.temp) - 273.15) * 9 / 5 + 32)*100)/100} °F`: 
-          `${Math.round((parseFloat(weather?.main.temp) - 273.15)*100)/100} °C`
-          }</p>
-        
-        <button onClick={handle_unit_changed}>Degree </button>
-      </div> 
-      :<div>oops</div>}
-    </>
-  )
-}
+      {weather ? (
+        <div className="card">
+          <h2>Weather App</h2>
+          <p>
+            {weather.name}, {weather.sys.country}
+          </p>
 
-export default CardWeather
+          <div className="weather">
+            <div className="image">
+              <img
+                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt="code"
+              />
+            </div>
+            <div className="weather-datos">
+              <p>&#34;{weather.weather[0].description}&#34;</p>
+              <p>
+                Wind speed : <span>{weather.wind.speed} m/s</span>
+              </p>
+              <p>
+                Clouds: <span>{weather.clouds.all} %</span>
+              </p>
+              <p>
+                Pressure: <span>{weather.main.pressure} hPa</span>
+              </p>
+            </div>
+          </div>
+
+          <p className="temperature">
+            {tempUnit == 'c'
+              ? `${
+                  Math.round(
+                    (((parseFloat(weather?.main.temp) - 273.15) * 9) / 5 + 32) *
+                      100
+                  ) / 100
+                } °F`
+              : `${
+                  Math.round((parseFloat(weather?.main.temp) - 273.15) * 100) /
+                  100
+                } °C`}
+          </p>
+
+          <button onClick={handle_unit_changed}>Degree </button>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
+};
+
+export default CardWeather;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <img
+        className="loading-image"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/b6e0b072897469.5bf6e79950d23.gif"
+        alt="loading imag"
+      />
+    </div>
+  );
+};
+
+export default Loading;
